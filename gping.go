4dbcb5adc81d384e00654ff1a87e6baef75858ab
Luther Barnum <luther.barnum@gmail.com>
2017-01-10 11:10:14 -0500

    Added code to handle Stdin and specifying file input

--- gping.go
+++ gping.go
@@ -5,34 +5,42 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
-func readLines(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
-
 // main This program is used to run "ps -ef | grep <process>"
 func main() {
-	file := "testfile"
-	contents, err := readLines(file)
-	if err != nil {
-		fmt.Print(err)
+	var stdInStat bool
+	stat, _ := os.Stdin.Stat()
+	if (stat.Mode() & os.ModeCharDevice) == 0 {
+		stdInStat = true
+		stdInScanner := bufio.NewScanner(os.Stdin)
+		for stdInScanner.Scan() {
+			fmt.Println(stdInScanner.Text())
+			if err := stdInScanner.Err(); err != nil {
+				fmt.Fprintln(os.Stderr, "reading standard input:", err)
+			}
+		}
+	}
+	var path string
+	if os.Args != nil && len(os.Args) > 1 {
+		path = os.Args[1]
+		//path := "testfile"
+		file, err := os.Open(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		var line string
+		for scanner.Scan() {
+			line = scanner.Text()
+			fmt.Printf("%s\n", line)
+		}
+	}
+	if os.Args != nil && len(os.Args) == 1 && stdInStat == false {
+		fmt.Fprintln(os.Stderr, "No input provided")
 	}
 
-	fmt.Printf("%T : %v\n", contents, contents)
-	for line := range contents {
-		fmt.Printf("%T : %v\n", contents[line], contents[line])
-	}
 }
