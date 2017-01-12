462025baeba6366d55055a37723150f063e7be94
Luther Barnum <luther.barnum@gmail.com>
2017-01-12 10:08:51 -0500

    Rewriting to just ping server and not check if it is valid

--- gping.go
+++ gping.go
@@ -6,42 +6,20 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-	"net"
 	"os"
 )
 
 // Server struct defines what is needed to ping servers
 type Server struct {
 	name     string
-	ip       string
-	valid    bool
 	pingable bool
 }
 
-func checkValidIP(ip string) bool {
-	trial := net.ParseIP(ip)
-	if trial.To4() == nil {
-		return false
-	}
+func (s *Server) isPingable() bool {
+	name := s.name
+	fmt.Printf("%s is pingable\n", name)
 	return true
 }
-
-func lookupIP(name string) string {
-	return name
-}
-
-func lookupname(ip string) string {
-	return ip
-}
-
-//func (p *Server) populate(ip string) bool {
-//trial := net.ParseIP(ip)
-//if trial.To4() == nil {
-//fmt.Printf("%v is not an IPv4 address\n", trial)
-//return false
-//}
-//return false
-//}
 
 // main This program is used to run "ps -ef | grep <process>"
 func main() {
@@ -69,25 +47,15 @@ func main() {
 		var line string
 		for scanner.Scan() {
 			line = scanner.Text()
-			if checkValidIP(line) {
-				s := Server{ip: line}
-				servers = append(servers, &s)
-			} else {
-				s := Server{name: line}
-				servers = append(servers, &s)
-			}
+			s := Server{name: line}
+			s.pingable = s.isPingable()
+			servers = append(servers, &s)
 		}
 	}
 	if os.Args != nil && len(os.Args) == 1 && stdInStat == false {
 		fmt.Fprintln(os.Stderr, "No input provided")
 	}
 	for _, server := range servers {
-		if checkValidIP(server.ip) {
-			(*server).valid = true
-		}
-		if server.name == "" {
-			(*server).name = server.ip
-		}
-		fmt.Printf("%s, %s, %v, %v\n", server.name, server.ip, server.valid, server.pingable)
+		fmt.Printf("%s, %v\n", server.name, server.pingable)
 	}
 }
